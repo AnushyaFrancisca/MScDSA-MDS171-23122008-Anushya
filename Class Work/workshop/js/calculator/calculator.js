59a6af82e0dfaa2e94c0d01e9fa46c6f9fdcc02b
AnushyaFranscisca <anushya.francisca@msds.christunniversity.in>
2024-01-10 19:29:37 +0530

    Addiing more codes

---
+++ Class Work/workshop/js/calculator/calculator.js
@@ -0,0 +1,19 @@
+function add(){
+    var a=document.getElementById("num1").value;
+    var b=document.getElementById("num2").value;
+    var c=parseInt(a)+parseInt(b);
+    document.getElementById("answer").value=c;
+
+}
+
+function subtract(){
+
+}
+
+function multiply(){
+
+}
+
+function divide(){
+
+}
